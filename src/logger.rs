f293dcd5ed353316714dde828f1b6b314c4c6e38
rtouch-dev <shahar.dayan.shaked@gmail.com>
2026-06-08 21:33:34 +0300

    formatted the logger file to be smaller and simular

--- src/logger.rs
+++ src/logger.rs
@@ -1,29 +1,24 @@
 use std::fs::{self, OpenOptions};
-use std::io::Write;
+use std::io::{Error, ErrorKind, Result, Write};
 use std::path::PathBuf;
 use std::time::SystemTime;
 
 pub struct Logger;
 
 impl Logger {
-    pub fn log(file_path: &str, message: &str) -> std::io::Result<()> {
-        let mut path = PathBuf::new();
-
-        if file_path.starts_with("~/") {
-            let home = match home::home_dir() {
-                Some(p) => p,
-                None => {
-                    return Err(std::io::Error::new(
-                        std::io::ErrorKind::NotFound,
-                        "Could not determine the home directory",
-                    ));
-                }
-            };
-            path.push(home);
-            path.push(&file_path[2..]);
+    pub fn log(file_path: &str, message: &str) -> Result<()> {
+        let path = if let Some(stripped) = file_path.strip_prefix("~/") {
+            let mut home = home::home_dir().ok_or_else(|| {
+                Error::new(
+                    ErrorKind::NotFound,
+                    "Could not determine the home directory",
+                )
+            })?;
+            home.push(stripped);
+            home
         } else {
-            path.push(file_path);
-        }
+            PathBuf::from(file_path)
+        };
 
         if let Some(parent) = path.parent() {
             if !parent.as_os_str().is_empty() {
@@ -32,13 +27,9 @@ impl Logger {
         }
 
         let mut file = OpenOptions::new().create(true).append(true).open(path)?;
-
-        let timestamp = SystemTime::now();
-        let log_line = format!("{:?}: {}\n", timestamp, message);
+        let log_line = format!("{:?}: {}\n", SystemTime::now(), message);
 
         file.write_all(log_line.as_bytes())?;
-        file.flush()?;
-
-        Ok(())
+        file.flush()
     }
 }
