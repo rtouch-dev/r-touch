9eda0306aa4f227369daba38e8a21ff2e9292e08
rtouch-dev <shahar.dayan.shaked@gmail.com>
2026-06-10 17:40:15 +0300

    added "--no-log" to secretly write "homework" files
    still working on that, it currently only works if you create a parent directory aswell

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
 };
 mod logger; //the logging logic (logger.rs)
 mod logmgr; //log-manager (logmgr.rs)
-mod replace_dir;
+mod replace_dir; //the file that is taking care on replacing folders with files (take a look)
 fn main() {
     let args: Vec<String> = env::args().collect();
     let args = gen_path(&args).unwrap_or_else(|error| {
@@ -14,7 +14,7 @@ fn main() {
 
     let path = args.0;
     let create_parents = args.1;
-
+    let should_log = args.2;
     create(path, create_parents).unwrap_or_else(|error| {
         println!("{error}");
         std::process::exit(1);
@@ -28,25 +28,33 @@ fn main() {
     } else {
         //if DID create the folder
         let message = format!("File & parent folder created: {path}");
-        logmgr::log_manager(&message);
+        if should_log {
+            logmgr::log_manager(&message)
+        };
     }
 }
 
-fn gen_path(args: &[String]) -> Result<(&str, bool), String> {
+fn gen_path(args: &[String]) -> Result<(&str, bool, bool), String> {
     if args.len() < 2 {
         return Err("You need to pass in the path to the file.".to_string());
     }
 
     let mut create_parents = false;
     let mut path = "";
-
+    let mut should_log: bool = true;
     for arg in args.iter().skip(1) {
         //check if has got any arguments
         if arg == "-p" || arg == "--parents" {
             //if got the argument "-p" or "--parents":
             create_parents = true; //setting the bool to true
+        //added --no-log
+        //version 0.2.4
+        // FIX
+        } else if arg == "--no-log" {
+            should_log = false;
         } else {
             path = arg.as_str(); //else return it without touching
+            should_log = true;
         }
     }
 
@@ -55,7 +63,7 @@ fn gen_path(args: &[String]) -> Result<(&str, bool), String> {
         return Err("You need to pass in the path to the file.".to_string()); //return error (and then in main exit)
     }
 
-    Ok((path, create_parents)) //if passed all the shi above return Ok status with the bool of create parents and the path
+    Ok((path, create_parents, should_log)) //if passed all the shi above return Ok status with the bool of create parents and the path
 }
 
 fn create(path: &str, create_parents: bool) -> Result<(), String> {
@@ -73,7 +81,7 @@ fn create(path: &str, create_parents: bool) -> Result<(), String> {
         }
     }
     // FIX: The parent directory creation block ends here. We do not create the file inside this block
-    // because we want the file to be created even when `create_parents` is false.
+    // because we want the file to be created even when `create_parents` is false
 
     // Checking if the requested path is an existing directory on the disk
     if path_buf.is_dir() {
