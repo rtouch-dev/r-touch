d61e69daaeb1458ceb1581b19fc3d2dfcd34b6f9
rtouch-dev <shahar.dayan.shaked@gmail.com>
2026-06-12 23:18:44 +0300

    Update logmgr.rs

--- src/log/logmgr.rs
+++ src/log/logmgr.rs
@@ -1,16 +1,11 @@
 use super::logger;
-use dirs_next::data_local_dir;
-use std::path::PathBuf;
-
 pub fn log_manager(message: &str) {
-    // OS-correct log directory
-    let path: PathBuf = data_local_dir()
-        .unwrap_or_else(|| PathBuf::from(".")) // fallback
-        .join("R-touch")
-        .join("logs")
-        .join("r-touch.log");
+    let mut path = dirs_next::data_local_dir()
+        .unwrap_or_else(|| std::path::PathBuf::from("."));
 
-    if let Err(e) = logger::Logger::log(path.to_str().unwrap(), message) {
+    path = path.join("R-touch").join("logs").join("r-touch.log");
+
+    if let Err(e) = logger::Logger::log(path.to_str().unwrap(), &message) {
         eprintln!("Error logging the action. Error: {e}");
         std::process::exit(1);
     }
