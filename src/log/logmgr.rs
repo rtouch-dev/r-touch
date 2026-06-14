a80de59f7e8c86b616bc20b99952888cce4037a1
rtouch-dev <shahar.dayan.shaked@gmail.com>
2026-06-14 22:58:58 +0300

    added error logging option (coming soon, already in progress)

--- src/log/logmgr.rs
+++ src/log/logmgr.rs
@@ -1,12 +1,21 @@
 use super::logger;
-pub fn log_manager(message: &str) {
-    let mut path = dirs_next::data_local_dir()
-        .unwrap_or_else(|| std::path::PathBuf::from("."));
+pub fn success_log(message: &str) {
+    let mut path = dirs_next::data_local_dir().unwrap_or_else(|| std::path::PathBuf::from("."));
 
     path = path.join("R-touch").join("logs").join("r-touch.log");
 
     if let Err(e) = logger::Logger::log(path.to_str().unwrap(), &message) {
         eprintln!("Error logging the action. Error: {e}");
+        std::process::exit(1);
+    }
+}
+#[rustfmt::skip]
+pub fn error_log(message: &str) {
+    let mut path = dirs_next::data_local_dir().unwrap_or_else(|| std::path::PathBuf::from("."));
+    path = path.join("R-touch").join("logs").join("crashes").join("r-touch_err.log");
+
+    if let Err(e) = logger::Logger::log(path.to_str().unwrap(), &message) {
+        eprintln!("Error logging the failer. Error: {e}");
         std::process::exit(1);
     }
 }
