0b94cedaa03564022c9e92bb72fe09c653b70eae
rtouch-dev <shahar.dayan.shaked@gmail.com>
2026-07-04 18:59:41 +0300

    Improve log path fallback and error message
    Make log directory fallback platform-independent.
    
    This change brings in platform-specific values for OS_ROOT, allowing for a better fallback directory for log files in cases where dirs_next::data_local_dir() fails.
    Also, a minor error was fixed, replacing 'failer' with 'failure' in the error logging message.

--- src/log/logmgr.rs
+++ src/log/logmgr.rs
@@ -1,6 +1,13 @@
 use super::logger;
+#[cfg(target_family = "windows")]
+const OS_ROOT: &str = "C:\\Users\\Public";
+#[cfg(target_family = "unix")]
+const OS_ROOT: &str = "/var/tmp";
+#[cfg(not(any(target_family = "windows", target_family = "unix")))]
+const OS_ROOT: &str = ".";
+
 pub fn success_log(message: &str) {
-    let mut path = dirs_next::data_local_dir().unwrap_or_else(|| std::path::PathBuf::from("."));
+    let mut path = dirs_next::data_local_dir().unwrap_or_else(|| std::path::PathBuf::from(OS_ROOT));
 
     path = path.join("R-touch").join("logs").join("r-touch.log");
 
@@ -9,9 +16,9 @@ pub fn success_log(message: &str) {
         std::process::exit(1);
     }
 }
-// Version 0.2.5
+
 pub fn error_log(message: &str) {
-    let mut path = dirs_next::data_local_dir().unwrap_or_else(|| std::path::PathBuf::from("."));
+    let mut path = dirs_next::data_local_dir().unwrap_or_else(|| std::path::PathBuf::from(OS_ROOT));
     path = path
         .join("R-touch")
         .join("logs")
@@ -19,7 +26,7 @@ pub fn error_log(message: &str) {
         .join("r-touch_err.log");
 
     if let Err(e) = logger::Logger::log(path.to_str().unwrap(), &message) {
-        eprintln!("Error logging the failer. Error: {e}");
+        eprintln!("Error logging the failure. Error: {e}");
         std::process::exit(1);
     }
 }
