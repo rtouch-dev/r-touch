5e8cc813f9120284420ef4b2134e507fb6763960
rtouch-dev <shahar.dayan.shaked@gmail.com>
2026-07-09 00:01:29 +0300

    restored comments (oops)

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use std::env;
 use std::process::ExitCode;
 
 mod log {
-    pub mod logger;
-    pub mod logmgr;
+    pub mod logger; //logging locic
+    pub mod logmgr; // logging manager (wrapper)
 }
-mod replace_dir;
+mod replace_dir; //the file that is taking care on replacing folders with files (take a look)
 
 struct TouchArgs<'a> {
     paths: Vec<Cow<'a, str>>,
@@ -33,11 +33,16 @@ fn main() -> ExitCode {
             log::logmgr::error_log(&format!("Unexpected Error : {error}"));
             return;
         });
-
+        //logging section
+        // println!("Success!");
         if !touch_args.create_parents && touch_args.should_log {
+            //if created a file in a regular path (in an existing dir) and didn't run with --no-log
             log::logmgr::success_log(&format!("File Created: {path}"));
-        } else if touch_args.should_log {
-            log::logmgr::success_log(&format!("File & parent folder created: {path}"))
+        } else {
+            //if DID create the folder
+            if touch_args.should_log {
+                log::logmgr::success_log(&format!("File & parent folder created: {path}"))
+            };
         }
     }
 
@@ -52,24 +57,24 @@ fn gen_path(args: &[String]) -> Result<TouchArgs<'_>, String> {
     let mut create_parents = false;
     let mut paths = Vec::new();
     let mut should_log: bool = true;
-
     for arg in args.iter().skip(1) {
+        //check if has got any arguments
         if arg == "-p" || arg == "--parents" {
             create_parents = true;
         } else if arg == "--no-log" {
             should_log = false;
         } else {
-            paths.push(Cow::Borrowed(arg.as_str()));
+            paths.push(Cow::Borrowed(arg.as_str())); //else return it without touching
         }
     }
 
     if paths.is_empty() {
+        //if used "-p" argument but didn't pass it a file (only a parent dir)
         log::logmgr::error_log(&format!(
             "Error: passed in parent folder, expected parent dir + file."
         ));
-        return Err("You need to pass in the path to the file.".to_string());
+        return Err("You need to pass in the path to the file.".to_string()); //return error (and then in main exit)
     }
-
     #[cfg(target_family = "windows")]
     {
         for path in &mut paths {
@@ -78,7 +83,6 @@ fn gen_path(args: &[String]) -> Result<TouchArgs<'_>, String> {
             }
         }
     }
-
     Ok(TouchArgs {
         paths,
         create_parents,
@@ -90,23 +94,27 @@ fn create(path: &str, create_parents: bool) -> Result<(), String> {
     let path_buf = std::path::Path::new(path);
 
     if create_parents {
+        //if the bool from the function above is true
         if let Some(parent) = path_buf.parent() {
             if !parent.as_os_str().is_empty() {
                 fs::create_dir_all(parent).map_err(|e| {
                     let err_msg = e.to_string();
                     log::logmgr::error_log(&err_msg);
-                    err_msg
+                    err_msg //here returning the error formatted to print out in line 22
                 })?;
             }
         }
     }
     if path_buf.is_dir() {
+        //if attempt to create a file in a name of an existing folder
         if let Err(e) = replace_dir::replace(path) {
             let err_msg = format!("Faild to replace direcrory : {e}");
             log::logmgr::error_log(&err_msg);
             return Err(err_msg);
         }
     } else {
+        // If the path is not an existing directory (the standard case for creating a new file),
+        // the code falls into this block and creates the file on the disk
         File::create(path).map_err(|e| {
             let err_msg = e.to_string();
             log::logmgr::error_log(&err_msg);
@@ -114,5 +122,5 @@ fn create(path: &str, create_parents: bool) -> Result<(), String> {
         })?;
     }
 
-    Ok(())
+    Ok(()) //if passed all the shi above return Ok status
 }
